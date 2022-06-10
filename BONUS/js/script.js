b1059c78b6ddf7c2f7d40c17e4796c27b55f2b00
Marco Braccini <marco.braccini@live.it>
2022-06-10 11:15:07 +0200

    BONUS - Testing javascript - Add logo select fx

---
+++ BONUS/js/script.js
@@ -0,0 +1,263 @@
+function logotxtShow1() {
+    var item1 = document.getElementById("logotxt1");
+    var hero = document.getElementById("logotxthero");
+    var item2 = document.getElementById("logotxt2");
+    var item3 = document.getElementById("logotxt3");
+    var item4 = document.getElementById("logotxt4");
+    var item5 = document.getElementById("logotxt5");
+    var item6 = document.getElementById("logotxt6");
+    var item7 = document.getElementById("logotxt7");
+    var item8 = document.getElementById("logotxt8");
+        if (item1.style.display === "none") {
+            item1.style.display = "block";
+            hero.style.display = "none";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        } else {
+            item1.style.display = "none";
+            hero.style.display = "flex";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        }
+}
+
+function logotxtShow2() {
+    var item2 = document.getElementById("logotxt2");
+    var hero = document.getElementById("logotxthero");
+    var item1 = document.getElementById("logotxt1");
+    var item3 = document.getElementById("logotxt3");
+    var item4 = document.getElementById("logotxt4");
+    var item5 = document.getElementById("logotxt5");
+    var item6 = document.getElementById("logotxt6");
+    var item7 = document.getElementById("logotxt7");
+    var item8 = document.getElementById("logotxt8");
+        if (item2.style.display === "none") {
+            item2.style.display = "block";
+            hero.style.display = "none";
+            item1.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        } else {
+            item2.style.display = "none";
+            hero.style.display = "flex";
+            item1.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        }
+}
+
+function logotxtShow3() {
+    var item3 = document.getElementById("logotxt3");    
+    var hero = document.getElementById("logotxthero");
+    var item1 = document.getElementById("logotxt1");
+    var item2 = document.getElementById("logotxt2");
+    var item4 = document.getElementById("logotxt4");
+    var item5 = document.getElementById("logotxt5");
+    var item6 = document.getElementById("logotxt6");
+    var item7 = document.getElementById("logotxt7");
+    var item8 = document.getElementById("logotxt8");
+        if (item3.style.display === "none") {
+            item3.style.display = "block";
+            hero.style.display = "none";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        } else {
+            item3.style.display = "none";
+            hero.style.display = "flex";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        }
+}
+
+function logotxtShow4() {
+    var item4 = document.getElementById("logotxt4");    
+    var hero = document.getElementById("logotxthero");
+    var item1 = document.getElementById("logotxt1");
+    var item2 = document.getElementById("logotxt2");
+    var item3 = document.getElementById("logotxt3");
+    var item5 = document.getElementById("logotxt5");
+    var item6 = document.getElementById("logotxt6");
+    var item7 = document.getElementById("logotxt7");
+    var item8 = document.getElementById("logotxt8");
+        if (item4.style.display === "none") {
+            item4.style.display = "block";
+            hero.style.display = "none";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        } else {
+            item4.style.display = "none";
+            hero.style.display = "flex";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        }
+}
+
+function logotxtShow5() { 
+    var item5 = document.getElementById("logotxt5");
+    var hero = document.getElementById("logotxthero");
+    var item1 = document.getElementById("logotxt1");
+    var item2 = document.getElementById("logotxt2");
+    var item3 = document.getElementById("logotxt3");
+    var item4 = document.getElementById("logotxt4");
+    var item6 = document.getElementById("logotxt6");
+    var item7 = document.getElementById("logotxt7");
+    var item8 = document.getElementById("logotxt8");
+        if (item5.style.display === "none") {
+            item5.style.display = "block";
+            hero.style.display = "none";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        } else {
+            item5.style.display = "none";
+            hero.style.display = "flex";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        }
+}
+
+function logotxtShow6() { 
+    var item6 = document.getElementById("logotxt6");
+    var hero = document.getElementById("logotxthero");
+    var item1 = document.getElementById("logotxt1");
+    var item2 = document.getElementById("logotxt2");
+    var item3 = document.getElementById("logotxt3");
+    var item4 = document.getElementById("logotxt4");
+    var item5 = document.getElementById("logotxt5");
+    var item7 = document.getElementById("logotxt7");
+    var item8 = document.getElementById("logotxt8");
+        if (item6.style.display === "none") {
+            item6.style.display = "block";
+            hero.style.display = "none";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        } else {
+            item6.style.display = "none";
+            hero.style.display = "flex";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item7.style.display = "none";
+            item8.style.display = "none";
+        }
+}
+
+function logotxtShow7() { 
+    var item7 = document.getElementById("logotxt7");
+    var hero = document.getElementById("logotxthero");
+    var item1 = document.getElementById("logotxt1");
+    var item2 = document.getElementById("logotxt2");
+    var item3 = document.getElementById("logotxt3");
+    var item4 = document.getElementById("logotxt4");
+    var item5 = document.getElementById("logotxt5");
+    var item6 = document.getElementById("logotxt6");
+    var item8 = document.getElementById("logotxt8");
+        if (item7.style.display === "none") {
+            item7.style.display = "block";
+            hero.style.display = "none";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item8.style.display = "none";
+        } else {
+            item7.style.display = "none";
+            hero.style.display = "flex";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item8.style.display = "none";
+        }
+}
+
+function logotxtShow8() { 
+    var item8 = document.getElementById("logotxt8");
+    var hero = document.getElementById("logotxthero");
+    var item1 = document.getElementById("logotxt1");
+    var item2 = document.getElementById("logotxt2");
+    var item3 = document.getElementById("logotxt3");
+    var item4 = document.getElementById("logotxt4");
+    var item5 = document.getElementById("logotxt5");
+    var item6 = document.getElementById("logotxt6");
+    var item7 = document.getElementById("logotxt7");
+        if (item8.style.display === "none") {
+            item8.style.display = "block";
+            hero.style.display = "none";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+        } else {
+            item8.style.display = "none";
+            hero.style.display = "flex";
+            item1.style.display = "none";
+            item2.style.display = "none";
+            item3.style.display = "none";
+            item4.style.display = "none";
+            item5.style.display = "none";
+            item6.style.display = "none";
+            item7.style.display = "none";
+        }
+}
